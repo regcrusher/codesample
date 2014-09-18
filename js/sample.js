0b7fff540dc9c03819daed56467baf3a27f2f6c7
Reggie Granados <rgranados@gmail.com>
2014-09-17 21:44:05 -0700

    Added CSS3 transition for password container

--- js/sample.js
+++ js/sample.js
@@ -4,24 +4,29 @@ $(function() {
 	var $password = $('#password_container');
 	var $errorbox = $('#messageBox1');
 
-	setHeight();
+	setTop();
 
-	// Call setHeight on all resize events
+	// Call setTop on all resize events
 	$(window).resize(function() {
-		setHeight();
+		setTop();
+
 	});
 
-	function setHeight() {
+	// Set the top of password and error fields on resize, so it's always
+	// v-centered in window.
+	function setTop() {
 		var h = $(window).height();
-		console.log($password.outerHeight());
+
 		$password.css('top', h/2 -($password.outerHeight()/2) + "px");
 		$errorbox.css('top', h/2 -($password.outerHeight()/2) + 15 + "px");
 
 
 	}
 
+
+
 	// Validate required fields.
-	// Getcode.php validates a field from the db
+	// Getcode.php validates a field from the db.
 	$("#password-form").validate({
 		errorPlacement: function(error, element) {
 			$("#messageBox1").html('');
@@ -48,7 +53,7 @@ $(function() {
 		},
 		// On Submit, fade out before submission.
 		submitHandler: function(form) {
-			$('#password').fadeOut('slow', function() {
+			$('#password_container').fadeOut(2000, function() {
 				form.submit();
 			});
 		}
